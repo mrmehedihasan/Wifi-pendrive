1147d654a680c893302e4e0361c719b7c09a955d
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2023-05-16 12:26:21 +0600

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,12 @@
+
+@echo off
+echo Wait... Wifi password saving in progress
+setlocal enabledelayedexpansion
+for /f "tokens=2delims=:" %%a in ('netsh wlan show profile ^|findstr ":"') do (
+    set "ssid=%%~a"
+    call :getpwd "%%ssid:~1%%"
+)
+:getpwd
+set "ssid=%*"
+for /f "tokens=2delims=:" %%i in ('netsh wlan show profile name^="%ssid:"=%" key^=clear ^| findstr /C:"Key Content"') do  echo ssid: %ssid% pass: %%i >> password.txt
+echo Success....
